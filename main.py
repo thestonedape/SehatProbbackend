229960b9cda530a2621431953dde9a34ae83e8a5
thestonedape <n1shanntjha@gmail.com>
2025-07-14 06:20:16 +0530

    port

--- main.py
+++ main.py
@@ -143,5 +143,6 @@ async def root() -> RootResponse:
     )
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os, uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
